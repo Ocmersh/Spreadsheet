4ed46b45509cf0159e99640873b29e93c8bd16b5
Ocmersh <Ocmersh@gmail.com>
2018-02-08 21:42:33 -0700

    Final Commit

--- Spreadsheet/Formula/Formula.cs
+++ Spreadsheet/Formula/Formula.cs
@@ -20,8 +20,7 @@ namespace Formulas
     public struct Formula
     {
         private IEnumerable<string> userForm;
-
-
+    
         /// <summary>
         /// Creates a Formula from a string that consists of a standard infix expression composed
         /// from non-negative floating-point numbers (using C#-like syntax for double/int literals), 
